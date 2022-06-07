ac8ddc509051aa6b9f24099d197e434551d66d1c
Kamva <kayndwanya24@gmail.com>
2022-06-07 23:42:49 +0200

    added files

---
+++ beneficiary.js
@@ -0,0 +1,17 @@
+ var fname= document.querySelector(".fname")
+ var lname= document.querySelector(".lname")
+ var id= document.querySelector(".id")
+ var msg= document.querySelector(".message")
+
+var firstName= "Mitlotlo"
+var lastName= "Nkonyana"
+
+let message = `Welcome <strong>${firstName} ${lastName}</strong> thank you for joining us, you are now part of the <strong>FLY HIGH Burial Society!</strong>
+<br><br>
+We can offer you more than this, you can add up to 10 family members ad reasonable price.
+<br><br>
+Please feel free to add your beneficiaries should anything happen to them, they will be covered.`
+msg.innerHTML= message;
+
+{/* <button class="button"><a href="index.html"></a>Home</button>
+<button class="button"><a href="index.html"></a>Add Beneficiary</button> */}
