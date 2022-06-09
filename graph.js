09b6188804f12a326e82f7478760d38166460347
ThamsanqaMpontshane <thamsanqalm01@gmail.com>
2022-06-09 16:00:16 +0200

    commit

---
+++ graph.js
@@ -0,0 +1,71 @@
+function Graph(data){
+    var counter1 = 0;
+    var counter2 = 0;
+    var counter3 = 0;
+    data = [
+        {
+            "fullName": "Sapho",
+            "IDnr": "9710225805089",
+            "dob": 24,
+            "Gender": "Male",
+            "claimDate": "01/September/2022"
+        },
+        {
+            "fullName": "Sapho",
+            "IDnr": "9507125803089",
+            "dob": 26,
+            "Gender": "Male",
+            "claimDate": "01/September/2022"
+        },
+        {
+            "fullName": "Sapho",
+            "IDnr": "0111233587123",
+            "dob": 21,
+            "Gender": "Male",
+            "claimDate": "01/September/2022"
+        },
+        {
+            "fullName": "Sapho",
+            "IDnr": "0111233583123",
+            "dob": 21,
+            "Gender": "Male",
+            "claimDate": "01/September/2022"
+        },
+        {
+            "fullName": "Sapho",
+            "IDnr": "011235438087",
+            "dob": 20,
+            "Gender": "Female"
+        },
+        {
+            "fullName": "Sapho",
+            "IDnr": "011235438087",
+            "dob": 20,
+            "Gender": "Female"
+        },
+        {
+            "fullName": "Sapho",
+            "IDnr": "011235438087",
+            "dob": 20,
+            "Gender": "Female"
+        },
+        {
+            "fullName": "Sapho",
+            "IDnr": "011235438087",
+            "dob": 20,
+            "Gender": "Female"
+        },
+        {
+            "fullName": "Sapho",
+            "IDnr": "011235438087",
+            "dob": 20,
+            "Gender": "Female"
+        }
+    ];
+    // loop over this array
+    //if the value of property value is bigger than and smaller than 30 increase counter of counter1
+
+
+
+
+};
