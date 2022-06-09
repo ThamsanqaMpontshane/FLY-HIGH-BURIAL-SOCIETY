454113b8520b992d21f08fb1176e0910bcc8303c
sapho-sys <saphonkunzi@gmail.com>
2022-06-09 10:16:03 +0200

    Project Complete!

--- logic.js
+++ logic.js
@@ -173,6 +173,20 @@ function addEntries(myObject) {
 }
 
 
+// function countAllFromTown(list){
+//     var count=0;
+//     //var counter=0;
+    
+//     var town=list.split(',');
+//     for(var i=0; i < town.length;i++){
+//       var regNo=town[i];
+//       if(regNo.startsWith('CL'))
+//         count++;
+//       }
+//     return count;
+//   }
+
+
 
 // first create main user/client
 // need to approve
